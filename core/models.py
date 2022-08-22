91209eb838950be7809e5ba0bd82dbfc5645b932
Ricardo André <ricardo.andre.ifc@gmail.com>
2022-08-21 22:30:09 -0300

    continuando o projeto

--- core/models.py
+++ core/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = "Autores"
+
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
@@ -14,3 +25,19 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros"
+    )
+
+    def __str__(self):
+        return f"{self.titulo} ({self.quantidade})"
