0e2910e99d18326726c294b20e4045ec020b075b
Ricardo André <ricardo.andre.ifc@gmail.com>
2022-08-28 22:12:51 -0300

    uso do SimpleJWT para autenticação no DRF

--- livraria/settings.py
+++ livraria/settings.py
@@ -98,6 +98,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
         "rest_framework.permissions.DjangoModelPermissions",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ]
 }
 
