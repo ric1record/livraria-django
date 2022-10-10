4b79ffdc67bfb2f7edd0ef0378d810faa92a60bc
Ricardo André <ricardo.andre.ifc@gmail.com>
2022-10-10 09:24:44 -0300

    dump e load de dados

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from media.models import Image
+
 from .autor import Autor
 from .categoria import Categoria
 from .editora import Editora
@@ -8,14 +10,22 @@ from .editora import Editora
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
-    quantidade = models.IntegerField()
-    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    quantidade = models.IntegerField(default=1)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     autores = models.ManyToManyField(Autor, related_name="livros")
     categoria = models.ForeignKey(
         Categoria, on_delete=models.PROTECT, related_name="livros"
     )
     editora = models.ForeignKey(
         Editora, on_delete=models.PROTECT, related_name="livros"
+    )
+    capa = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        default=None,
     )
 
     def __str__(self):
